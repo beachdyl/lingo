3974df7a5de86e27071bedc62d95f0f829130710
Dylan Bowerman <12419551+beachdyl@users.noreply.github.com>
2021-11-29 21:23:43 -0500

    refine for this use, modify template, intial message handling

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
 // Require the necessary discord.js classes
 const fs = require('fs');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
 const { Client, Collection, Intents } = require('discord.js');
-const { token, clientId, guildId } = require('./config.json');
+const { token, clientId, guildId, channelId } = require('./config.json');
+const errHandle = require ('./errorHandler.js')
+
+// Try deleting old errorTemp.txt if it exists
+try {fs.unlinkSync('./errorTemp.txt');}
+catch (error) {}
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+	presence: {
+		activity: {
+			type: "PLAYING", name: "with a dictionary"
+		}
+	}
+});
 
+// Register commands from commands directory
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
-	console.log(`${command.data.name} registered as command.`);
+try {
+	for (const file of commandFiles) {
+		const command = require(`./commands/${file}`);
+		client.commands.set(command.data.name, command);
+	}
+} catch (error) {
+	try {
+		errHandle(`Command registration of command named ${command.data.name}\n${error}`, 1, client);
+	} catch (error) {
+		errHandle(`Command registration of unknown command\n${error}`, 1, client);
+	}
 }
+//console.log(client.commands);
 
+client.on("messageCreate", async message => {
+	if (channelId == message.channel.id) console.log(message.content);
+  });
+
+// Process slash command interactions
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
@@ -24,35 +48,53 @@ client.on('interactionCreate', async interaction => {
 
 	if (!command) return;
 
-	if (interaction.commandName === 'crash') client.user.setPresence({status: 'idle'});
+	// If interaction is restart command, do things
+	if (interaction.commandName === 'restart') {
+		client.user.setPresence({status: 'idle'});
+		errHandle(`Requested restart`, 8, client)
+	}
 
 	try {
 		await command.execute(interaction);
 	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command! Check the console.', ephemeral: true });
+		try {
+			errHandle(`Interaction named ${interaction.commandName}\n${error}`, 1, client);
+			await interaction.reply({ content: 'There was an error while executing this command! Please alert a Dylan.', ephemeral: true });
+		} catch (error) {
+			try {
+				errHandle(`Error of interaction named ${interaction.commandName}\n${error}`, 5, client);
+			} catch (error) {
+				errHandle(`Error of interaction of unknown name\n${error}`, 5, client);
+			}
+		}
 	}
 });
 
+// Process button interactions
 client.on('interactionCreate', interaction => {
 	if (!interaction.isButton()) return;
-	console.log(interaction);
+	errHandle(interaction, 1, client);
 });
 
+// Do things once the bot is ready
 client.on('ready', () => {
+	// Set the bot status to online
 	client.user.setPresence({status: 'online'});
+
+	// Check for persistent errors
+	try {
+		fs.accessSync('./persistError.txt');
+		errHandle(fs.readFileSync('./persistError.txt'), 7, client);
+	} catch {}
 });
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+// Check for unhandled errors on each interaction
+client.on('interactionCreate', interaction => {
+	try {
+		fs.accessSync('./tempError.txt');
+		errHandle(fs.readFileSync('./tempError.txt'), 3, client);
+	} catch {}
+});
 
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
-// Login to Discord with your client's token
+// Login to Discord using the secret token
 client.login(token);
