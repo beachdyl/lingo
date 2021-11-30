3edd48ef6a4ffa787edf309be77a93d8dbdda649
Dylan Bowerman <12419551+beachdyl@users.noreply.github.com>
2021-11-30 04:24:58 -0500

    idek what changed, hopefully it was smart

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const errHandle = require ('./errorHandler.js');
 const { channelId } = require('./config.json');
 
-// matchWord function. Returns a value, usually 0 or 1, for how many matches there are in the words list
+// matchWord function. Returns a value, usually 0 or 1, for how many matches there are of the first argument in the words list
 let matchWord = async function(word) {
 	let length;
 	let answer = await new Promise((resolve) => {
@@ -27,6 +27,7 @@ let matchWord = async function(word) {
 	return length;
 };
 
+// matchUsed function. Returns a value, usually 0 or 1, for how many matches there are of the first argument in the list of words already used
 let matchUsed = async function(word) {
 	let length;
 	let answer = await new Promise((resolve) => {
@@ -50,6 +51,7 @@ let matchUsed = async function(word) {
 	return length;
 };
 
+// Allow us to get the ID (as a promise result) of the user that said a word. The word passed in must have already been said.
 let whoSaid = async function(word) {
 	let answer = await new Promise((resolve) => {
 		const regEx = new RegExp('\\b'+word+'\\b', "i")
@@ -63,6 +65,7 @@ let whoSaid = async function(word) {
 					result.push(line);
 				};
 			});
+			// Converting the results into a promise-returned user ID string
 			result = result[0];
 			result = result.substring(result.indexOf(" ")+1, result.length);
 			resolve(result);
@@ -71,31 +74,34 @@ let whoSaid = async function(word) {
 	return answer;
 };
 
+// Allow us to get the score of users
 let getScore = function(userid) {
 	let userFile = `./files/users/${userid}.txt`
 
+	// If they have a score, get and return it, otherwise, return 0 for no existing score.
 	if (fs.existsSync(userFile)) {
-		console.log(fs.readFileSync(userFile), 'utf8');
 		return parseInt(fs.readFileSync(userFile), 10);
 	} else {
 		return 0;
 	};
 };
 
+// Process when a correct and accepted word is said
 let correctWord = async function(message) {
 	message.react('✅');
 	fs.appendFileSync('./files/Used.txt',`${message.content} ${message.author.id}\n`);
-	console.log('hi'+getScore(message.author.id))
 	fs.writeFileSync(`./files/users/${message.author.id}.txt`,`${getScore(message.author.id)+1}`);
 };
 
+// Process when a non-word is said, and delete it
 let deleteWord = async function(message) {
 	message.react('❓');
-	await new Promise (function(resolve) {setTimeout(resolve, 2700);});
+	await new Promise (function(resolve) {setTimeout(resolve, 2700);}); // Wait before deleting
 	message.delete();
-	fs.appendFileSync('./files/Wrong.txt',`${message.content} ${message.author.id}\n`);
+	fs.appendFileSync('./files/Wrong.txt',`${message.content} ${message.author.id}\n`); // Log the message and user for safety
 };
 
+// Process when a repeated word is said
 let wrongWord = async function(message) {
 	message.react('❌');
 };
