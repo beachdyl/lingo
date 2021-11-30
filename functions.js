3737c3f55e6052de8b47ed8a11ada78010de107a
Dylan Bowerman <12419551+beachdyl@users.noreply.github.com>
2021-11-30 04:10:48 -0500

    added user score stracking

--- functions.js
+++ functions.js
@@ -5,89 +5,99 @@ const { channelId } = require('./config.json');
 
 // matchWord function. Returns a value, usually 0 or 1, for how many matches there are in the words list
 let matchWord = async function(word) {
-    let length;
-    let answer = await new Promise((resolve) => {
-        const regEx = new RegExp('\\b'+word+'\\b', "i")
-        const result = [];
+	let length;
+	let answer = await new Promise((resolve) => {
+		const regEx = new RegExp('\\b'+word+'\\b', "i")
+		const result = [];
 
-        // Scan the file for the word
-        fs.readFile('./files/US.txt', 'utf8', function (err, contents) {
-            let lines = contents.toString().split("\n");
-            lines.forEach(line => {
-                if (line && line.search(regEx) >= 0) {
-                    result.push(line);
-                }
-            })
-            resolve(result);
-        })
-    }).then(value => {
-        // Return how many matches there are. Should only be 0 or 1.
-        length = value.length;
-    })
-    return length;
+		// Scan the file for the word
+		fs.readFile('./files/US.txt', 'utf8', function (err, contents) {
+			let lines = contents.toString().split("\n");
+			lines.forEach(line => {
+				if (line && line.search(regEx) >= 0) {
+					result.push(line);
+				};
+			});
+			resolve(result);
+		});
+	}).then(value => {
+		// Return how many matches there are. Should only be 0 or 1.
+		length = value.length;
+	});
+	return length;
 };
 
 let matchUsed = async function(word) {
-    let length;
-    let answer = await new Promise((resolve) => {
-        const regEx = new RegExp('\\b'+word+'\\b', "i")
-        const result = [];
+	let length;
+	let answer = await new Promise((resolve) => {
+		const regEx = new RegExp('\\b'+word+'\\b', "i")
+		const result = [];
 
-        // Scan the file for the word
-        fs.readFile('./files/Used.txt', 'utf8', function (err, contents) {
-            let lines = contents.toString().split("\n");
-            lines.forEach(line => {
-                if (line && line.search(regEx) >= 0) {
-                    result.push(line);
-                }
-            })
-            resolve(result);
-        })
-    }).then(value => {
-        // Return how many matches there are. Should only be 0 or 1.
-        length = value.length;
-    })
-    return length;
+		// Scan the file for the word
+		fs.readFile('./files/Used.txt', 'utf8', function (err, contents) {
+			let lines = contents.toString().split("\n");
+			lines.forEach(line => {
+				if (line && line.search(regEx) >= 0) {
+					result.push(line);
+				};
+			});
+			resolve(result);
+		});
+	}).then(value => {
+		// Return how many matches there are. Should only be 0 or 1.
+		length = value.length;
+	});
+	return length;
 };
 
 let whoSaid = async function(word) {
-    let answer = await new Promise((resolve) => {
-        const regEx = new RegExp('\\b'+word+'\\b', "i")
-        let result = [];
+	let answer = await new Promise((resolve) => {
+		const regEx = new RegExp('\\b'+word+'\\b', "i")
+		let result = [];
 
-        // Scan the file for the word
-        fs.readFile('./files/Used.txt', 'utf8', function (err, contents) {
-            let lines = contents.toString().split("\n");
-            lines.forEach(line => {
-                if (line && line.search(regEx) >= 0) {
-                    result.push(line);
-                }
-            })
-            result = result[0];
-            result = result.substring(result.indexOf(" ")+1, result.length);
-            resolve(result);
-        })
-    });
-    return answer;
+		// Scan the file for the word
+		fs.readFile('./files/Used.txt', 'utf8', function (err, contents) {
+			let lines = contents.toString().split("\n");
+			lines.forEach(line => {
+				if (line && line.search(regEx) >= 0) {
+					result.push(line);
+				};
+			});
+			result = result[0];
+			result = result.substring(result.indexOf(" ")+1, result.length);
+			resolve(result);
+		});
+	});
+	return answer;
+};
+
+let getScore = function(userid) {
+	let userFile = `./files/users/${userid}.txt`
+
+	if (fs.existsSync(userFile)) {
+		console.log(fs.readFileSync(userFile), 'utf8');
+		return parseInt(fs.readFileSync(userFile), 10);
+	} else {
+		return 0;
+	};
 };
 
 let correctWord = async function(message) {
-    console.log('congrats '+message.author.username);
-    message.react('✅');
-    fs.appendFileSync('./files/Used.txt',`${message.content} ${message.author.id}\n`);
+	message.react('✅');
+	fs.appendFileSync('./files/Used.txt',`${message.content} ${message.author.id}\n`);
+	console.log('hi'+getScore(message.author.id))
+	fs.writeFileSync(`./files/users/${message.author.id}.txt`,`${getScore(message.author.id)+1}`);
 };
 
 let deleteWord = async function(message) {
-    console.log('fool '+message.author.username);
-    message.react('❓');
-    await new Promise (function(resolve) {setTimeout(resolve, 2700);});
-    message.delete();
-    fs.appendFileSync('./files/Wrong.txt',`${message.content} ${message.author.id}\n`);
+	message.react('❓');
+	await new Promise (function(resolve) {setTimeout(resolve, 2700);});
+	message.delete();
+	fs.appendFileSync('./files/Wrong.txt',`${message.content} ${message.author.id}\n`);
 };
 
 let wrongWord = async function(message) {
-    console.log('fool '+message.author.username);
-    message.react('❌');
+	message.react('❌');
 };
 
-module.exports = { matchWord, matchUsed, correctWord, deleteWord, wrongWord, whoSaid } ;
+module.exports = { matchWord, matchUsed, correctWord, deleteWord, wrongWord, whoSaid, getScore } ;
